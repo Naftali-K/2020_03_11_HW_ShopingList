d2ede2058ae6382f888846d25d2f218e30d0a012
Naftali <naftali@bk.ru>
2020-03-13 22:24:18 +0200

    * Updated ProductList.java   - Add functionality of CheckBox.     - Add to list Product     - Remove Product from List   - Add function for print Toast (for testings)   - Change productList (that was choices) to JSONArray list   - Move back to MainActivity with result  and Extra (JSON object)
    * Add implementation to Gson
    
    * MainActivity - Add receiving result from temporary activity
      - Receiving extra and changing JSONArray back to ArrayList by Gson

--- app/src/main/java/com/example/shopinglist/MainActivity.java
+++ app/src/main/java/com/example/shopinglist/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.shopinglist;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,6 +8,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,5 +34,25 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //checking if same number request and result of actions OK
+        if(requestCode == NUMBER_REQUEST && resultCode == RESULT_OK){
+            //if everything ok, going to here
+            String stringFromExtra = data.getStringExtra("productList");
+
+            Gson gson = new Gson();
+            ArrayList<String> products = gson.fromJson(stringFromExtra, ArrayList.class);
+            //Toast.makeText(getBaseContext(), "num items: " + products.size(), Toast.LENGTH_LONG).show();
+            Toast.makeText(getBaseContext(), "items: " + products.get(0), Toast.LENGTH_LONG).show();
+
+        }else{
+            //if have any problem, so go to here
+
+        }
     }
 }
--- app/src/main/java/com/example/shopinglist/ProductList.java
+++ app/src/main/java/com/example/shopinglist/ProductList.java
@@ -3,14 +3,21 @@ package com.example.shopinglist;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class ProductList extends AppCompatActivity {
+
+    ArrayList<String> productList = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,28 @@ public class ProductList extends AppCompatActivity {
         RecyclerView rv = findViewById(R.id.rv_body);
         ProductListAdapter adapter = new ProductListAdapter();
         rv.setAdapter(adapter);
+
+        findViewById(R.id.btn_finish).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //toastPrint("Totally add: " + productList.size() + " items");//testing code
+
+                /*
+                For use JSONArray, need use like normal array.
+                To variable of JASONArray, you add value by value (or Object)
+                 */
+                JSONArray objectArray = new JSONArray();
+                for(String item: productList){
+                    objectArray.put(item);
+                }
+
+                Intent intent = new Intent();
+                intent.putExtra("productList", objectArray.toString());
+
+                setResult(Activity.RESULT_OK, intent);
+                finish();
+            }
+        });
 
     }
 
@@ -37,6 +66,25 @@ public class ProductList extends AppCompatActivity {
     public void onClickCheckBox(View view){
         boolean checked = ((CheckBox) view).isChecked();
         String text = ((CheckBox) view).getText().toString();
-        Toast.makeText(ProductList.this, text, Toast.LENGTH_LONG).show();
+        //toastPrint(text);
+        //Toast.makeText(ProductList.this, text, Toast.LENGTH_LONG).show();
+
+        if(checked){
+            //add new product to the list
+            //toastPrint("Add product: " + text);
+
+            productList.add(text);//add item/product to list
+        }else{
+            //remove product from list
+            //toastPrint("Remove product: " + text);
+
+            int indexOfProduct = productList.indexOf(text); //getting index product that was add to the list
+            productList.remove(indexOfProduct); //removing the item
+        }
+    }
+
+    //function for Toast
+    public void toastPrint(String text){
+        Toast.makeText(getBaseContext(), text, Toast.LENGTH_LONG).show();
     }
 }
